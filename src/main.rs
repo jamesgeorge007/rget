ef986b75f4ca4c6ad4b087410472e754b75da4ce
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-12-17 17:59:49 +0530

    add progress bar and colored output :+1:

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,26 @@ fn main() {
     let url = matches.value_of("URL").unwrap();
     println!("{}", url);    
 }
+
+fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = match quiet_mode {
+        true => ProgressBar::hidden(),
+        false => {
+            match length {
+                Some(len) => ProgressBar::new(len),
+                None => ProgressBar::new_spinner(),
+            }
+        }
+    };
+
+    bar.set_message(msg);
+    match length.is_some() {
+        true => bar
+            .set_style(ProgressStyle::default_bar()
+                .template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+                .progress_chars("=> ")),
+        false => bar.set_style(ProgressStyle::default_spinner()),
+    };
+
+    bar
+}
